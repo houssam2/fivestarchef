0e65e745546498b8f0f335ab71484d5650d1e716
mpmcauliffe <mcauliffe99@gmail.com>
2018-07-31 19:42:28 -0400

    3 routes successful

--- routes/app.js
+++ routes/app.js
@@ -11,6 +11,14 @@ app.get("/", (req, res) => {
     res.render("../views/home.ejs");
 });
 
+app.get("/reserve", (req, res) => {
+    res.render("../views/reserve.ejs");
+});
+
+app.get("/tables", (req, res) => {
+    res.render("../views/tables.ejs");
+});
+
 app.listen(3000, () => {
     console.log("Up on port 3000");
 })
