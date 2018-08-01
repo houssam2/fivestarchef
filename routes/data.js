44a645939270fa8e30072f0c4ca66c608d2d124b
Houssam Moustafa <houssam2@yahoo.com>
2018-07-31 21:24:30 -0400

    fixed data.js

--- routes/data.js
+++ routes/data.js
@@ -11,15 +11,15 @@ var waitList = {
     arr: []
 };
 
-function Person(custName, custPhone, custEmail, custId) {
-    this.tableId = tableI;
+function Person(custName, custPhone, custEmail, custId, tableId) {
+    this.tableId = tableId;
     this.custName = custName;
     this.custPhone = custPhone;
     this.custEmail = custEmail;
     this.custId = custId;
 }
 
-function ReadTables() {
+function readTables() {
     fs.readFile(tablesFile, "utf8", function(err, data) {
         if (err) { console.log("Error reading tables file!"); return; }
         var tables = JSON.parse(data);
@@ -27,14 +27,14 @@ function ReadTables() {
     });
 }
 
-function WriteTables() {
+function writeTables() {
     var obj = JSON.stringify(tables);
     fs.writeFile(tablesFile, obj, function(err) {
         if (err) { console.log("Error writing tables file!"); return; }
     });
 }
 
-function ReadWaitList() {
+function readWaitList() {
     fs.readFile(waitListFile, "utf8", function(err, data) {
         if (err) { console.log("Error reading waitList file!"); return; }
         var waitList = JSON.parse(data);
@@ -42,7 +42,7 @@ function ReadWaitList() {
     });
 }
 
-function WriteWaitList() {
+function writeWaitList() {
     var obj = JSON.stringify(waitList);
     fs.writeFile(waitListFile, obj, function(err) {
         if (err) { console.log("Error writing waitList file!"); return; }
@@ -58,16 +58,16 @@ module.exports = {
     writeWaitList: writeWaitList
 };
 
-// var t1 = new Person("ham1", "9171", "h1@y.com", "ham_a1"); tables.arr.push(t1);
-// var t2 = new Person("ham2", "9172", "h2@y.com", "ham_a2"); tables.arr.push(t2);
-// var t3 = new Person("ham3", "9173", "h3@y.com", "ham_a3"); tables.arr.push(t3);
-// var t4 = new Person("ham4", "9174", "h4@y.com", "ham_a4"); tables.arr.push(t4);
-// WriteTables();
-// ReadTables();
+var t1 = new Person(1, "ham1", "9171", "h1@y.com", "ham_a1"); tables.arr.push(t1);
+var t2 = new Person(2, "ham2", "9172", "h2@y.com", "ham_a2"); tables.arr.push(t2);
+var t3 = new Person(3, "ham3", "9173", "h3@y.com", "ham_a3"); tables.arr.push(t3);
+var t4 = new Person(4, "ham4", "9174", "h4@y.com", "ham_a4"); tables.arr.push(t4);
+writeTables();
+readTables();
 
-// var w1 = new Person("ham1", "9171", "h1@y.com", "ham_w1"); waitList.arr.push(w1);
-// var w2 = new Person("ham2", "9172", "h2@y.com", "ham_w2"); waitList.arr.push(w2);
-// var w3 = new Person("ham3", "9173", "h3@y.com", "ham_w3"); waitList.arr.push(w3);
-// var w4 = new Person("ham4", "9174", "h4@y.com", "ham_w4"); waitList.arr.push(w4);
-// WriteWaitList();
-// ReadWaitList();
+var w1 = new Person(-1, "ham1", "9171", "h1@y.com", "ham_w1"); waitList.arr.push(w1);
+var w2 = new Person(-1, "ham2", "9172", "h2@y.com", "ham_w2"); waitList.arr.push(w2);
+var w3 = new Person(-1, "ham3", "9173", "h3@y.com", "ham_w3"); waitList.arr.push(w3);
+var w4 = new Person(-1, "ham4", "9174", "h4@y.com", "ham_w4"); waitList.arr.push(w4);
+writeWaitList();
+readWaitList();
